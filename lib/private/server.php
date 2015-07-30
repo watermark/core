be54d2225d50d8ec5287043c1087d192c65b4bf6
Morris Jobke <hey@morrisjobke.de>
2015-07-30 13:57:04 +0200

    Add config option to update charset of mysql to utf8mb4
    * fully optional
    * requires additional options set in the database

--- lib/private/server.php
+++ lib/private/server.php
@@ -317,8 +317,8 @@ class Server extends SimpleContainer implements IServerContainer {
 			return new Hasher($c->getConfig());
 		});
 		$this->registerService('DatabaseConnection', function (Server $c) {
-			$factory = new \OC\DB\ConnectionFactory();
 			$systemConfig = $c->getSystemConfig();
+			$factory = new \OC\DB\ConnectionFactory($systemConfig);
 			$type = $systemConfig->getValue('dbtype', 'sqlite');
 			if (!$factory->isValidType($type)) {
 				throw new \OC\DatabaseException('Invalid database type');
