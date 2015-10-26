942e1371eef8b4cc45d24f51c63f127cf89985b1
Lukas Reschke <lukas@owncloud.com>
2015-10-27 00:53:54 +0100

    Serve files with an attachment disposition for new DAV endpoint
    This adds a `Content-Disposition: attachment` header to all files served via the DAV endpoint.

--- apps/dav/lib/server.php
+++ apps/dav/lib/server.php
@@ -7,6 +7,9 @@ use OCA\DAV\Connector\Sabre\BlockLegacyClientPlugin;
 use OCA\DAV\Files\CustomPropertiesBackend;
 use OCP\IRequest;
 use Sabre\DAV\Auth\Plugin;
+use Sabre\DAV\IFile;
+use Sabre\HTTP\RequestInterface;
+use Sabre\HTTP\ResponseInterface;
 use Sabre\HTTP\Util;
 
 class Server {
@@ -42,6 +45,14 @@ class Server {
 		if($request->isUserAgent(['/WebDAVFS/'])) {
 			$this->server->addPlugin(new \OCA\DAV\Connector\Sabre\FakeLockerPlugin());
 		}
+
+		// Serve all files with an Content-Disposition of type "attachment"
+		$this->server->on('beforeMethod', function (RequestInterface $requestInterface, ResponseInterface $responseInterface) {
+				$node = $this->server->tree->getNodeForPath($requestInterface->getPath());
+				if (($node instanceof IFile)) {
+					$responseInterface->addHeader('Content-Disposition', 'attachment');
+				}
+		});
 
 		// wait with registering these until auth is handled and the filesystem is setup
 		$this->server->on('beforeMethod', function () {
