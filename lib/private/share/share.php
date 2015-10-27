44f9dd544c23ac0c1b3137d3f62c0d4eafd196bc
Roeland Jago Douma <rullzer@owncloud.com>
2015-10-27 10:22:59 +0100

    Properly close db cursors

--- lib/private/share/share.php
+++ lib/private/share/share.php
@@ -1110,9 +1110,10 @@ class Share extends Constants {
 					->from('share')
 					->where($qb->expr()->eq('id', $qb->createParameter('id')))
 					->setParameter(':id', $rootItem['parent']);
-				$result = $qb->execute();
+				$dbresult = $qb->execute();
 
-				$result = $result->fetch();
+				$result = $dbresult->fetch();
+				$dbresult->closeCursor();
 				if (~(int)$result['permissions'] & $permissions) {
 					$message = 'Setting permissions for %s failed,'
 						.' because the permissions exceed permissions granted to %s';
@@ -1176,6 +1177,7 @@ class Share extends Constants {
 							$parents[] = $item['id'];
 						}
 					}
+					$result->closeCursor();
 				}
 
 				// Remove the permissions for all reshares of this item
@@ -1196,7 +1198,7 @@ class Share extends Constants {
 
 			/*
 			 * Permissions were added
-			 * Update all USERGROUP shares. (So group shares where the user moved his mountpoint).
+			 * Update all USERGROUP shares. (So group shares where the user moved their mountpoint).
 			 */
 			if ($permissions & ~(int)$rootItem['permissions']) {
 				$qb = $connection->getQueryBuilder();
@@ -1216,6 +1218,7 @@ class Share extends Constants {
 					$items[] = $item;
 					$ids[] = $item['id'];
 				}
+				$result->closeCursor();
 
 				// Add permssions for all USERGROUP shares of this item
 				if (!empty($ids)) {
